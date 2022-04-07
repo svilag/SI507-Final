02f3855fc6e5b2c40dfdfae71bae78994aa769ba
svilag <svilag@umich.edu>
2022-04-07 11:25:32 -0400

    start defining functions

--- source-code/507_final.py
+++ source-code/507_final.py
@@ -11,10 +11,11 @@ from bs4 import BeautifulSoup
 
 
 BASE_URL = 'https://wgi.org'
-PERCUSSION_URL = 'percussion/'
-GUARD_URL = 'color-guard/'
-WINDS_URL = 'winds/'
+# PERCUSSION_URL = 'percussion/'
+# GUARD_URL = 'color-guard/'
+# WINDS_URL = 'winds/'
 
+url_paths = {'percussion/', 'color-guard/', 'winds/'}
 score_years = {2018, 2019, 2020, 2021, 2022}
 
 # url patterns:
@@ -22,11 +23,48 @@ score_years = {2018, 2019, 2020, 2021, 2022}
 # perc-scores-yyyy
 # yyyy-percussion-scores
 
+def cache_pages():
+    """parses pages on wgi.com with scores data and caches the pages
+
+    params:
+        url(string): url to cache
+
+    returns:
+        cache
+    """
+    # TODO implement caching
+    for path in url_paths:
+        url = BASE_URL + path
+        for year in score_years:
+            try:
+                data = requests.get
+            finally:
+                data = requests.get
+            # TODO
+
+def parse_scores(data):
+    """parses pages on wgi.com with scores data"""
+    # TODO implement parser
+    pass
+
 
 @dataclass(frozen=True)
 class Percussion:
     """creates a percussion class object"""
-    group_type: string
+    group_type: str
+    name: str
+
+@dataclass(frozen=True)
+class Guard:
+    """creates a color guard class object"""
+    group_type: str
+    name: str
+
+@dataclass(frozen=True)
+class Winds:
+    """creates a winds class object"""
+    group_type: str
+    name: str
 
 
 
