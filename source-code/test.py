bb88731b3928376f55374e84148c06de036f0981
svilag <svilag@umich.edu>
2022-04-10 00:43:20 -0400

    Update test.py

--- source-code/test.py
+++ source-code/test.py
@@ -1,10 +1,15 @@
 import requests
+import re
 from bs4 import BeautifulSoup as bs
 
 url = 'https://wgi.org/percussion/perc-scores-2020/'
 req = requests.get(url).text
 
 soup = bs(req, 'html.parser')
-table_rows = soup.findAll('tr')
+table_rows = soup.find_all('tr') # list
 
-print(table_rows)
+recap = soup.find_all(string=re.compile("Recap"))
+print(recap)
+
+
+
